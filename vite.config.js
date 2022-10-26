f88cf5f6acc8eda3c1c21d7aaf7e9249ded5dcd8
lihe07 <li@imlihe.com>
2022-10-26 11:18:50 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
-import solidPlugin from 'vite-plugin-solid'
+import solid from 'vite-plugin-solid'
 import unocss from 'unocss/vite'
 
 export default defineConfig({
-  plugins: [solidPlugin(), unocss()],
-  server: {
-    port: 3000
-  },
+  plugins: [solid(), unocss()],
   build: {
     target: 'esnext'
   }
